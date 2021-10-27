7eba0f9c709c4cce0e801ec9ae914ae574155983
Zabil Maooz <zabil.maooz@ccjk.com>
2021-10-27 18:00:30 +0500

    Basic Add Counter Added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import { data } from './db';
 import './App.css';
 import Counters from './components/Counters';
+import AddCounter from './components/AddCounter';
 
 function App() {
   const [counters, setCounters] = useState(data);
 
   return (
     <div className="App">
+      <AddCounter counters={counters} setCounters={setCounters} />
       <Counters counters={counters} setCounters={setCounters} />
     </div>
   );
