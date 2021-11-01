5c656d23bfbbfb2eaecf5542cbacb78055c8f36b
Zabil Maooz <zabil.coder@gmail.com>
2021-11-01 20:16:38 +0500

    Form added

--- src/components/AddCounter.js
+++ src/components/AddCounter.js
@@ -1,13 +1,20 @@
 function AddCounter({counters, setCounters}) {
-    const newCounter = () => {
+    const addNewCounter = (e) => {
+        e.preventDefault();
         let counter = {
             "id": counters.length+1,
-            "name": "Dunkrik",
-            "value": 2
+            "name": e.target.counterName.value,
+            "value": parseInt(e.target.counterValue.value)
         }
         setCounters([counter, ...counters]);
     };
-    return ( <button onClick={newCounter}>Add Counter</button> );
+    return ( 
+    <form onSubmit={addNewCounter}>
+        <input name="counterName" placeholder="Enter name" type="text" />
+        <input name="counterValue" placeholder="Enter counter value" type="number" />
+        <button>Add Counter</button>
+    </form>
+     );
 }
 
 export default AddCounter;
